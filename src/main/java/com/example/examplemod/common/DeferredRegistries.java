4845ce33b1d41f12c3054b430c7de6d9d7a6e31f
Daan <Daan.Schaer@Gmail.com>
2021-05-11 22:17:46 +0200

    Init

---
+++ src/main/java/com/example/examplemod/common/DeferredRegistries.java
@@ -0,0 +1,11 @@
+package com.example.examplemod.common;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class DeferredRegistries {
+
+}
